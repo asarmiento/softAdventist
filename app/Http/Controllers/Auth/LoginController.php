d50fb7b7ee9a06c1bc31b72383069cac6df40e9b
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2017-07-11 17:44:10 -0600

    redirigir

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      * @var string
      */
 
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/tesoreria/home';
 
     /**
      * Create a new controller instance.
