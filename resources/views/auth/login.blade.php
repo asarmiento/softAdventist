f4e63f6705559eca2249f75b9c1b738220d26cfe
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2017-08-01 09:04:39 -0600

    ruta contacto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
         <!-- LOGIN FORM -->
         <!--===================================================-->
         <div class="cls-content">
-            <div class="pull-right "><a class="btn btn-default" href="">Contactanos <i class="fa fa-send"></i></a></div>
+            <div class="pull-right "><a class="btn btn-default" href="{{route('contact')}}">Contactanos <i class="fa fa-send"></i></a></div>
             <div class="cls-content-sm panel">
 
                 <div class="panel-body">
