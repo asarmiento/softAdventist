dbe214ee84b62d1eb1e87f90c6c33f60f5140f0c
Anwar Sarmiento <anwarsarmiento@gmail.com>
2018-08-29 12:47:16 -0600

    cambio reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/registrado/inscription';
+    protected $redirectTo = '/ja';
 
     /**
      * Create a new controller instance.
