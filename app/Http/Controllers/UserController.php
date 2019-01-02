6a2ff11ab5e7c8f6432bc7c6ecc477b855652512
Anwar Sarmiento <anwarsarmiento@gmail.com>
2019-01-02 12:50:59 -0600

    fix: corrección de nombre de vista de blade en mayuscula

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 {
     public function create()
     {
-        return view('auth.CreateUser');
+        return view('auth.createUser');
     }
 
     public function labelSelect()
